9c216bff4270c218d9882e837253ff1b6a500267
kreedz <kreedz@users.noreply.github.com>
2018-07-18 21:29:52 +0500

    get chapters (subtitles) and write them to files

--- src/notabenoid.ts
+++ src/notabenoid.ts
@@ -1,40 +1,49 @@
 import axios from 'axios';
 import * as cheerio from 'cheerio';
+import { writeFile } from 'fs';
+import { join } from 'path';
+
 // const args: string[] = process.argv.slice(2);
 // --directory /hello/js --bookId 56565
-
-// const uploadDir: string = join('./');
-// const files: string[] = [
-//     '369809',
-//     '369785'
-// ];
-const baseUrl: string = 'https://opennota.duckdns.org/';
-const defaultBookId: string = '69614';
-const getBookPartUrl = (bookId: string) => `book/${bookId}/`;
-const partBookUrl: string = getBookPartUrl(defaultBookId);
+const baseUrl = 'https://opennota.duckdns.org';
+const defaultBookId = '69614';
+const getBookPartUrl = (bookId: string) => `/book/${bookId}/`;
+const partBookUrl = getBookPartUrl(defaultBookId);
 const bookUrl = `${baseUrl}${partBookUrl}`;
-// const params: string = 'algorithm=0&skip_neg=0&skip_neg=1&author_id=0&format=s&enc=UTF-8&crlf=1';
-// const getFileUrl = (fileName: string) => `${baseUrl}${partBookUrl}${fileName}/download?${params}`;
+const params = '/download?algorithm=0&skip_neg=0&skip_neg=1&author_id=0&format=s&enc=UTF-8&crlf=1';
+const uploadDir = join('./');
 
-async function getBookData() {
+async function getUrls() {
     try {
         const bookHtmlStr = await axios(bookUrl);
         const $ = cheerio.load(bookHtmlStr.data);
         return $('table#Chapters tr td:nth-child(2) a').toArray().map(a => {
-            const href = $(a).attr('href').split('/');
-            return href[href.length - 1];
+            const href = $(a).attr('href');
+            return baseUrl + href + params;
         });
-
     } catch (e) {
         console.error(e);
     }
 }
 
-getBookData().then(data => console.log(data));
+async function writeChapter(url: string) {
+    const chapter = await axios(url);
+    const disposition = chapter.headers['content-disposition'];
+    const [, fileName] = disposition.match(/filename="(.+)"/);
+    writeFile(join(uploadDir, fileName), chapter.data, err => {
+        if (err) {
+            throw err;
+        }
+    });
+}
 
-// get(bookUrl, bookResponse => {
-//     files.forEach((fileName: string) => {
-//         const file: WriteStream = createWriteStream(join(`${uploadDir}${fileName}.srt`));
-//         const request = get(getFileUrl(fileName), (response) => response.pipe(file));
-//     });
-// });
+async function writeChapters() {
+    try {
+        const urls = await getUrls();
+        urls.forEach(url => writeChapter(url));
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+writeChapters();
