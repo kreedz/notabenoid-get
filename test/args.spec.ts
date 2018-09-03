125c9236c58bce98c4e4a87a4c214397cd9f8e03
kreedz <kreedz@users.noreply.github.com>
2018-09-03 19:49:50 +0500

    update args test

--- test/args.spec.ts
+++ test/args.spec.ts
@@ -1,5 +1,5 @@
-import { Args, defaultDir, EArgKeys, IArgs } from '../src/Args';
 import { RequiredArgumentError } from '../src/errors/RequiredArgumentError';
+import { defaultDir, EArgKeys, IArgs, ParseArgs } from '../src/ParseArgs';
 
 describe('Arguments', () => {
     const processArgv = ['/path/to/node', '/path/to/command'];
@@ -9,34 +9,34 @@ describe('Arguments', () => {
 
     test('should give an object that contains dir field and url field with string value', () => {
         const url = 'http://some.not.validated.url';
-        const args = new Args();
-        const received = args.getArgs(getProcessArgvWith(url));
+        const args = new ParseArgs(getProcessArgvWith(url));
+        const received = args.getArgs();
         const expected = getExpectedArgs({ url });
         expect(received).toEqual(expected);
     });
 
     test('should throw RequiredArgumentError if arguments not given', () => {
-        const received = () => new Args().getArgs();
+        const received = () => new ParseArgs(getProcessArgvWith()).getArgs();
         const expected = RequiredArgumentError;
         expect(received).toThrow(expected);
     });
 
     test('should throw RequiredArgumentError if bookId or url arguments not given', () => {
-        const received = () => new Args().getArgs(getProcessArgvWith('--someArg=arg'));
+        const received = () => new ParseArgs(getProcessArgvWith('--someArg=arg')).getArgs();
         const expected = RequiredArgumentError;
         expect(received).toThrow(expected);
     });
 
     test('should give an object that contains dir field and bookId field with string value of numbers', () => {
         const bookId = '4567';
-        const received = new Args().getArgs(getProcessArgvWith(bookId));
+        const received = new ParseArgs(getProcessArgvWith(bookId)).getArgs();
         const expected = getExpectedArgs({ bookId });
         expect(received).toEqual(expected);
     });
 
     test('should give an object that contains dir field and url field with string value', () => {
         const url = 'http://some.not.validated.url';
-        const received = new Args().getArgs(getProcessArgvWith(url));
+        const received = new ParseArgs(getProcessArgvWith(url)).getArgs();
         const expected = getExpectedArgs({ url });
         expect(received).toEqual(expected);
     });
@@ -44,7 +44,7 @@ describe('Arguments', () => {
     test('should give an object that contains dir field', () => {
         const dir = 'subtitles/eng';
         const bookId = '4567';
-        const received = new Args().getArgs(getProcessArgvWith(`${EArgKeys.DIR}=${dir}`, bookId));
+        const received = new ParseArgs(getProcessArgvWith(`${EArgKeys.DIR}=${dir}`, bookId)).getArgs();
         const expected = getExpectedArgs({ dir, bookId });
         expect(received).toEqual(expected);
     });
